58b7d2953563051f93c74d686f642ffee422d8d9
Saranga Chamod <91281208+SarangaChamod@users.noreply.github.com>
2024-04-08 23:27:05 +0530

    profile_index_1_v.1.2

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import Navigation from './Components/navigation.jsx'
-import Index_1 from './All_Components/Profiles/index_1.jsx';
+import Index_1 from './All_Components/Profiles/profile_index_1.jsx';
 
 export default function App() {
   return (
